fd38114b2adbd5708ae197feccafe3dfcc9f7fea
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-03-12 02:42:26 +0600

    Front,title name work done

---
+++ src/app/work/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'Work Us',
+    description:'This is work page.'
+}
+const WorkPage = () => {
+    return (
+        <div>
+            <h1>Work Page</h1>
+        </div>
+    );
+};
+
+export default WorkPage;
